03e700db1d6c49d3bcf2f1d46509cb7d28ec64b3
Markel <markelfdz8@gmail.com>
2025-07-19 19:04:19 +0200

    model,queries,controllers productos

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -30,7 +30,7 @@ const getAllProductss = async (req, res) => {
     }
 }
 
-const getProductsById = async (req, res) => {
+const getProductoById = async (req, res) => {
     const { id } = req.body;
 
     try {
@@ -53,4 +53,109 @@ const getProductsById = async (req, res) => {
         console.log("Error en registro:", error);
         return res.status(500).json({ ok: false, error: "Error interno del servidor" });
     }
+}
+
+const getProductosByType = async (req, res) => {
+    const { tipo } = req.body;
+
+    try {
+
+        const products = await getProductsByType(tipo);
+        if (!products) {
+            return res.status(404).json({
+                ok: false,
+                error: "No existe ningun producto con ese tipo"
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                error: "Productos encontrados",
+                products
+            })
+        }
+
+    } catch (error) {
+        console.log("Error en registro:", error);
+        return res.status(500).json({ ok: false, error: "Error interno del servidor" });
+    }
+}
+
+const createProducto = async (req, res) => {
+    const { nombre, descripcion, tipo, unidad } = req.body;
+
+    try {
+        const products = await createProduct(nombre, descripcion, tipo, unidad);
+        if (!products) {
+            return res.status(404).json({
+                ok: false,
+                error: "No se ha podido crear el producto"
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                error: "Productos encontrados",
+                products
+            })
+        }
+
+    } catch (error) {
+        console.log("Error en registro:", error);
+        return res.status(500).json({ ok: false, error: "Error interno del servidor" });
+    }
+}
+
+const updateProductoById = async (req, res) => {
+    const { nombre, descripcion, tipo, unidad, id } = req.body;
+
+    try {
+        const products = await updateProductById(nombre, descripcion, tipo, unidad, id);
+        if (!products) {
+            return res.status(404).json({
+                ok: false,
+                error: "No se ha podido modificar el producto"
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                error: "Producto modificado",
+                products
+            })
+        }
+
+    } catch (error) {
+        console.log("Error en registro:", error);
+        return res.status(500).json({ ok: false, error: "Error interno del servidor" });
+    }
+}
+
+const deleteProductoById = async (req, res) => {
+    const { id } = req.body;
+
+    try {
+        const products = await deleteProductById(id);
+        if (!products) {
+            return res.status(404).json({
+                ok: false,
+                error: "No se ha podido eliminar el producto"
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                error: "Producto eliminado",
+                products
+            })
+        }
+    } catch (error) {
+        console.log("Error en registro:", error);
+        return res.status(500).json({ ok: false, error: "Error interno del servidor" });
+    }
+}
+
+module.exports = {
+    getAllProductss,
+    getProductoById,
+    getProductosByType,
+    createProducto,
+    updateProductoById,
+    deleteProductoById
 }
--- src/queries/productQueries.js
+++ src/queries/productQueries.js
@@ -4,31 +4,31 @@ const products = {
      * QUERIE: 1. Recibir producto por id
      */
     getProductById: `
-            SELECT * FROM products 
-            WHERE id_product = $1
+            SELECT * FROM productos 
+            WHERE id_producto = $1
             `,
 
     /**
      *  QUERIE: 2. Recibir productos por tipo 
      * */
     getProductsByType: `
-            SELECT * FROM products 
-            WHERE type = $1
+            SELECT * FROM productos 
+            WHERE tipo = $1
             `,
 
     /**
      * QUERIE: 3. Recibir todos los productos
      */
     getAllProducts: `
-            SELECT * FROM products
+            SELECT * FROM productos
             `,
 
     /**
      * QUERIE: 4. Insertar nuevo producto
      */
     insertProducts: `
-                    INSERT INTO products (name, description, type, unity, cuantity) 
-                    VALUES($1, $2, $3, $4, $5) 
+                    INSERT INTO productos (nombre, descripcion, tipo, unidad) 
+                    VALUES($1, $2, $3, $4) 
                     RETURNING *
                     `,
 
@@ -36,8 +36,8 @@ const products = {
      * QUERIE: 5. Eliminar un producto por id 
      *  */
     deleteProductById: `
-                    DELETE FROM products 
-                    WHERE id_product = $1
+                    DELETE FROM productos 
+                    WHERE id_producto = $1
                     RETURNING *
                     `,
 
@@ -45,8 +45,8 @@ const products = {
      * QUERIE: 6. Modificar un producto por id
      */
     updateProductById: `
-                    UPDATE products SET name = $1, description = $2, type = $3, unity = $4, cuantity = $5
-                    WHERE id_product = $6
+                    UPDATE productos SET nombre = $1, descripcion = $2, tipo = $3, unidad = $4
+                    WHERE id_producto = $5
                     RETURNING *
                     `,
 }
