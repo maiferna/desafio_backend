1634685c091155b9bf724c74e848d38e13ff1497
Markel <markelfdz8@gmail.com>
2025-07-20 12:01:42 +0200

    model,queries,controllers,routes productos

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Importar subrutas
+const authRoutes = require('./authRoutes');
+const userRoutes = require('./userRoutes');
+const productRoutes = require('./products.routes')
+
+router.use('/auth', authRoutes);             // login y registro
+router.use('/users', userRoutes);
+router.use("/products", productRoutes);
+
+// EXPORTS
+module.exports = router;
