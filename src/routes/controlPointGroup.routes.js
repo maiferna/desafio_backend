eb07bed607818b09b38cd0728404256b37cddae2
Markel <markelfdz8@gmail.com>
2025-07-21 19:44:12 +0200

    controlPoints

--- src/routes/controlPointGroup.routes.js
+++ src/routes/controlPointGroup.routes.js
@@ -8,19 +8,59 @@ const {
     deleteControlPointGroupController
 } = require('../controllers/controlPointGroupController');
 
+const { check } = require("express-validator");
+const { validateJwt, validateRole, validateInput } = require("../middlewares/index.js");
+
+// Middleware común solo para admins
+const adminAccess = [validateJwt, validateRole("admin")];
+
 // GET /api/v1/control-point-groups
-router.get('/', getAllControlPointGroupsController);
+router.get('/', [
+    adminAccess
+], getAllControlPointGroupsController);
 
 // GET /api/v1/control-point-groups/:id
-router.get('/:id', getControlPointGroupByIdController);
+router.get('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    validateInput,
+    adminAccess
+], getControlPointGroupByIdController);
 
 // POST /api/v1/control-point-groups
-router.post('/', createControlPointGroupController);
+router.post('/', [
+    check("nombre", "nombre invalido")
+        .notEmpty()
+        .withMessage('nombre esta vacio'),
+    check("figura", "figura invalido")
+        .notEmpty()
+        .withMessage('figura esta vacio'),
+    validateInput,
+    adminAccess
+], createControlPointGroupController);
 
 // PUT /api/v1/control-point-groups/:id
-router.put('/:id', updateControlPointGroupController);
+router.put('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    check("nombre", "nombre invalido")
+        .notEmpty()
+        .withMessage('nombre esta vacio'),
+    check("figura", "figura invalido")
+        .notEmpty()
+        .withMessage('figura esta vacio'),
+    validateInput,
+    adminAccess
+], updateControlPointGroupController);
 
 // DELETE /api/v1/control-point-groups/:id
-router.delete('/:id', deleteControlPointGroupController);
+router.delete('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    adminAccess
+], deleteControlPointGroupController);
 
 module.exports = router;
--- src/routes/controlPointState.routes.js
+++ src/routes/controlPointState.routes.js
@@ -8,19 +8,60 @@ const {
     deleteControlPointStateController
 } = require('../controllers/controlPointStateController');
 
+const { check } = require("express-validator");
+const { validateJwt, validateRole, validateInput } = require("../middlewares/index.js");
+
+// Middleware común solo para admins
+const adminAccess = [validateJwt, validateRole("admin")];
+
 // GET /api/v1/control-point-states
-router.get('/', getAllControlPointStatesController);
+router.get('/', [
+    adminAccess
+], getAllControlPointStatesController);
 
 // GET /api/v1/control-point-states/:id
-router.get('/:id', getControlPointStateByIdController);
+router.get('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    validateInput,
+    ...adminAccess
+], getControlPointStateByIdController);
 
 // POST /api/v1/control-point-states
-router.post('/', createControlPointStateController);
+router.post('/', [
+    check("nombre", "nombre invalido")
+        .notEmpty()
+        .withMessage('nombre esta vacio'),
+    check("color", "color invalido")
+        .notEmpty()
+        .withMessage('color esta vacio'),
+    validateInput,
+    ...adminAccess
+], createControlPointStateController);
 
 // PUT /api/v1/control-point-states/:id
-router.put('/:id', updateControlPointStateController);
+router.put('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    check("nombre", "nombre invalido")
+        .notEmpty()
+        .withMessage('nombre esta vacio'),
+    check("color", "color invalido")
+        .notEmpty()
+        .withMessage('color esta vacio'),
+    validateInput,
+    ...adminAccess
+], updateControlPointStateController);
 
 // DELETE /api/v1/control-point-states/:id
-router.delete('/:id', deleteControlPointStateController);
+router.delete('/:id', [
+    check("id", "ID invalido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    validateInput,
+    ...adminAccess
+], deleteControlPointStateController);
 
 module.exports = router;
