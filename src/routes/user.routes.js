abe3f6fb798fdad6bb42601dec7ab364ad128fab
Eibel86 <afc72455@gmail.com>
2025-07-21 13:33:55 +0200

    validate user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,7 +16,9 @@ const adminAccess = [validateJwt, validateRole("admin")];
 
 
 // GET /api/v1/users  obtener todos los usuarios
-router.get("/", adminAccess, getUsersController);
+router.get("/", [
+    adminAccess
+], getUsersController);
 
 
 // GET /api/v1/users/email/:email  obtener usuario por email
@@ -24,10 +26,10 @@ router.get("/email/:email", [
 
     check("email", "Email inválido").notEmpty()
         .withMessage('El email no puede estar vacío')
-        .isEmail()
-        .withMessage('El formato del email no es correcto')
         .isLength({ min: 3, max: 100 })
-        .withMessage('Debe tener entre 3 y 100 caracteres'),
+        .withMessage('Debe tener entre 3 y 100 caracteres')
+        .isEmail()
+        .withMessage('El formato del email no es correcto'),
     validateInput,
     ...adminAccess
 ], getUserByEmailController);
@@ -35,42 +37,46 @@ router.get("/email/:email", [
 
 // GET /api/v1/users/:id_usuario → obtener usuario por ID
 router.get("/:id_usuario", [
-    ...adminAccess,
-    check("id_usuario", "ID inválido").isInt({ min: 1 }),
-    validateInput
+    check("id_usuario", "ID inválido").notEmpty()
+        .isInt({ min: 1 })
+        .withMessage('La id debe ser un numero entero como minimo 1'),
+    validateInput,
+    ...adminAccess
 ], getUserByIdController);
 
 
 // PUT /api/v1/users/:id_usuario  actualizar usuario por ID
 router.put("/:id_usuario", [
-    ...adminAccess,
-    check("id_usuario", "ID inválido").isInt({ min: 1 }),
-    check("name", "Nombre requerido y debe ser una cadena").optional().isString().isLength({ min: 3, max: 100 }),
-    check("email", "Email inválido").optional().isEmail(),
-    check("password", "La contraseña debe tener mínimo 6 caracteres, una mayúscula y un número")
-        .optional()
-        .isStrongPassword({
-            minLength: 6,
-            minUppercase: 1,
-            minNumbers: 1,
-            minSymbols: 0
-        }),
-    check("role", "El rol debe ser admin, tecnico o cliente").optional().isIn(["admin", "tecnico", "cliente"]),
+    // ...adminAccess,
+    // check("id_usuario", "ID inválido").notEmpty()
+    //     .isInt({ min: 1 })
+    //     .withMessage('La id debe ser un numero entero como minimo 1'),
+    // check("name", "Nombre requerido y debe ser una cadena").optional().isString().isLength({ min: 3, max: 100 }),
+    // check("email", "Email inválido").optional().isEmail(),
+    // check("password", "La contraseña debe tener mínimo 6 caracteres, una mayúscula y un número")
+    //     .optional()
+    //     .isStrongPassword({
+    //         minLength: 6,
+    //         minUppercase: 1,
+    //         minNumbers: 1,
+    //         minSymbols: 0
+    //     }),
+    // check("role", "El rol debe ser admin, tecnico o cliente").optional().isIn(["admin", "tecnico", "cliente"]),
 
-    // Validación cruzada personalizada
-    check("id_cliente").custom((id_cliente, { req }) => {
-        if (req.body.role === "cliente") {
-            if (!id_cliente) {
-                throw new Error("id_cliente es obligatorio si el rol es cliente");
-            }
-            if (!Number.isInteger(id_cliente) || id_cliente <= 0) {
-                throw new Error("id_cliente debe ser un número entero positivo");
-            }
-        }
-        return true;
-    }),
+    // // Validación cruzada personalizada
+    // check("id_cliente").custom((id_cliente, { req }) => {
+    //     if (req.body.role === "cliente") {
+    //         if (!id_cliente) {
+    //             throw new Error("id_cliente es obligatorio si el rol es cliente");
+    //         }
+    //         if (!Number.isInteger(id_cliente) || id_cliente <= 0) {
+    //             throw new Error("id_cliente debe ser un número entero positivo");
+    //         }
+    //     }
+    //     return true;
+    // }),
 
-    validateInput
+    // validateInput
 ], putUserByIdController);
 
 
