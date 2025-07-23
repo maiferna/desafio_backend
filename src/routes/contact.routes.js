e9b6b728f669bb75dafb2b288b4f2722c34fda7d
sebas <sebastian07051999@gmail.com>
2025-07-23 10:34:04 +0200

    ADD: implementación de los errores personalizados en el formulario de contacto

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const router = express.Router();
-
+const { check } = require("express-validator");
+const { validateInput } = require("../middlewares");
 const { handleContactForm } = require("../controllers/contactController");
 
-router.post("/", handleContactForm);
+const router = express.Router();
+
+router.post(
+    "/",
+    [
+        check("name", "El nombre es obligatorio").notEmpty().isLength({ min: 2, max: 100 }),
+        check("email", "Email inválido").notEmpty().isEmail(),
+        check("message", "El mensaje no puede estar vacío").notEmpty().isLength({ min: 5 }),
+        validateInput
+    ],
+    handleContactForm
+);
 
 module.exports = router;
